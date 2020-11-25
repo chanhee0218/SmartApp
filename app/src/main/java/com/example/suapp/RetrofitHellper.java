83ad61570ad2a5f775a7bf9c3bd3343972683821
chanhee0218 <5144eqq02@gmail.com>
2020-11-25 19:40:09 +0900

    final commit

---
+++ app/src/main/java/com/example/suapp/RetrofitHellper.java
@@ -0,0 +1,11 @@
+package com.example.suapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RetrofitHellper {
+    final String Base_URL="http://3.35.16.85:20000/";
+    @GET("/")
+    Call<String> Auth();
+
+}
--- app/src/main/java/com/example/suapp/SecondFragment.java
+++ app/src/main/java/com/example/suapp/SecondFragment.java
@@ -1,27 +1,38 @@
 package com.example.suapp;
 
-import android.content.Context;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
+import android.widget.Button;
+import android.util.Base64;
+import android.widget.VideoView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import static java.lang.Byte.decode;
 
 public class SecondFragment extends Fragment {
-    private ProgressBar progressBar;
-    static final int PERMISSION_REQUEST_CODE=1;
-    private File outputfile,path;
-    MediaPlayer player;
-    ImageButton imageButton;
+    Button stopbtn,runbtn,retrobtn;
+    VideoView videoView;
+
 
     public SecondFragment() {
         // Required empty public constructor
@@ -37,26 +48,71 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_second, container, false);
-        imageButton=view.findViewById(R.id.imgbutton);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        stopbtn = view.findViewById(R.id.stop);
+        runbtn = view.findViewById(R.id.start);
+        retrobtn = view.findViewById(R.id.retrobtn);
+        videoView = view.findViewById(R.id.videoview);
+        retrobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context context=v.getContext();
-                player = MediaPlayer.create(context, R.raw.downloadimg);
-                player.start();
-                player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                Retrofit retrofit = getRetrofit();
+                RetrofitHellper retrofitHellper = retrofit.create(RetrofitHellper.class);
+                retrofitHellper.Auth().enqueue(new Callback<String>() {
                     @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.stop();
-                        mp.release();
+                    public void onFailure(Call<String> call, Throwable t) {
+                        System.out.println((t.toString()));
                     }
-                });
 
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        byte[] video = Base64.getDecoder().decode(response.body());
+                        System.out.println(Arrays.toString(video));
+                        System.out.println(video.length);
+                        File root = Environment.getExternalStorageDirectory();
+                        String path = root.getAbsolutePath() + "/test.mp4";
+                        System.out.println(path);
+                        File file = new File(path);
+
+                        try {
+                            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+                            fileOutputStream.write(video);
+                            fileOutputStream.flush();
+                            fileOutputStream.close();
+                            videoView.setVideoPath(path);
+                            playVideo();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                });
             }
         });
         return view;
+    }
+    Retrofit getRetrofit(){
+        Gson gson=new GsonBuilder().setLenient().create();
+        return new Retrofit.Builder()
+                .baseUrl("http://3.35.16.85:20000/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+    }
+    public void StartButton(View v){
+        playVideo();
+    }
+    public void StopButton(View v){
+        stopVideo();
+    }
 
-        }
+    private void stopVideo() {
+        videoView.pause();
+
+    }
+
+    private void playVideo() {
+        videoView.start();
+    }
 }
